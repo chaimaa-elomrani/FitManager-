35838d381a1927b64f6e2ea3f3ae8bed0d678e00
Chaima elomrani <chaimaelomrani6@gmail.com>
2025-12-03 15:17:08 +0100

    separated the logic of courses function from the index.php

--- config/config.php
+++ config/config.php
@@ -14,7 +14,7 @@ try {
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-    echo "safi connected!!!";
+    // echo "safi connected!!!";
 } catch (PDOException $e) {
     echo "matconnectach" . $e->getMessage();
     exit();
